0d6df21d4eb36e0b9e2c34a0108de2370d4d485b
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-04-28 11:46:47 +0100

    Fixed base image for baton with iRODS 3.3.1.

--- base/irods-3.3.1/setup.sh
+++ base/irods-3.3.1/setup.sh
@@ -1,79 +1,82 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
+set -e -o pipefail
+LOG_LOCATION="log"
+userCommands=$*
 
-logLocation="log"
-
-function exitWithError {
-    >&2 cat $logLocation
-
-    if [[ -n "$DEBUG" ]]
-    then
-        if [[ "$DEBUG" -eq 1 ]]
-        then
-            echo "Opening bash shell for debugging"
-            bash
-        fi
-    fi
-    exit 1
+execute_user_command() {
+    echo "Executing user command: $*" >> ${LOG_LOCATION}
+    bash -c "$userCommands"
+    exit $?
 }
 
-if [[ -z "$IRODS_SETTINGS" ]]
+exitWithError() {
+    # Log to stderr
+    >&2 cat ${LOG_LOCATION}
+
+    if [ -n "${DEBUG}" ] && [ "${DEBUG}" -eq 1 ]
+    then
+        >&2 echo "Debug mode enabled - ignoring settings issue"
+        execute_user_command
+    else
+        exit 1
+    fi
+}
+
+if [ -z "${IRODS_SETTINGS}" ]
 then
     errorMessage="IRODS_SETTINGS not set"
-    echo "$errorMessage" >> $logLocation
+    echo "${errorMessage}" >> ${LOG_LOCATION}
     exitWithError
 fi
 
-if [[ ! -f "$IRODS_SETTINGS" ]]
+if [ ! -f "${IRODS_SETTINGS}" ]
 then
-    echo "$IRODS_SETTINGS has not been mounted in a volume" >> $logLocation
+    echo "${IRODS_SETTINGS} has not been mounted in a volume" >> ${LOG_LOCATION}
 
-    if [[ -z "$IRODS_USERNAME" ]]
+    if [ -z "${IRODS_USERNAME}" ]
     then
         missingVariable="IRODS_USERNAME"
-    elif [[ -z "$IRODS_HOST" ]]
+    elif [ -z "${IRODS_HOST}" ]
     then
         missingVariable="IRODS_HOST"
-    elif [[ -z "$IRODS_PORT" ]]
+    elif [ -z "${IRODS_PORT}" ]
     then
         missingVariable="IRODS_PORT"
-    elif [[ -z "$IRODS_ZONE" ]]
+    elif [ -z "${IRODS_ZONE}" ]
     then
         missingVariable="IRODS_ZONE"
     fi
 
-    if [[ -n "$missingVariable" ]]
+    if [ -n "${missingVariable}" ]
     then
-        settingsDirectory="$(dirname $IRODS_SETTINGS)"
-        settingsFile="$(basename $IRODS_SETTINGS)"
-        errorMessage="$missingVariable environment variable must be set. Alternatively, mount a directory containing an iRODs client settings file '$settingsFile' in '$settingsDirectory/'"
-        echo "$errorMessage" >> $logLocation
+        settingsDirectory="$(dirname ${IRODS_SETTINGS})"
+        settingsFile="$(basename ${IRODS_SETTINGS})"
+        errorMessage="${missingVariable} environment variable must be set. Alternatively, mount a directory containing an iRODs client settings file '${settingsFile}' in '${settingsDirectory}/'"
+        echo "${errorMessage}" >> ${LOG_LOCATION}
         exitWithError
     else
-        echo "irodsUserName $IRODS_USERNAME" >> $IRODS_SETTINGS
-        echo "irodsHost $IRODS_HOST" >> $IRODS_SETTINGS
-        echo "irodsPort $IRODS_PORT" >> $IRODS_SETTINGS
-        echo "irodsZone $IRODS_ZONE" >> $IRODS_SETTINGS
-        echo "Written settings to: $IRODS_SETTINGS" >> $logLocation
+        echo "irodsUserName ${IRODS_USERNAME}" >> ${IRODS_SETTINGS}
+        echo "irodsHost ${IRODS_HOST}" >> ${IRODS_SETTINGS}
+        echo "irodsPort ${IRODS_PORT}" >> ${IRODS_SETTINGS}
+        echo "irodsZone ${IRODS_ZONE}" >> ${IRODS_SETTINGS}
+        echo "Written settings to: ${IRODS_SETTINGS}" >> $LOG_LOCATION
     fi
 else
-    echo "$IRODS_SETTINGS has been mounted in a volume" >> $logLocation
+    echo "${IRODS_SETTINGS} has been mounted in a volume" >> $LOG_LOCATION
 fi
 
-cat $IRODS_SETTINGS >> $logLocation
+cat ${IRODS_SETTINGS} >> $LOG_LOCATION
 
-if [[ -n "$IRODS_PASSWORD" ]]
+if [ -n "${IRODS_PASSWORD}" ]
 then
-    echo "iRODs password supplied to authenticate user '$IRODS_USERNAME'" >> $logLocation
-    iinit "$IRODS_PASSWORD" 2>> $logLocation
-    if [[ $? -ne 0 ]]
+    echo "iRODs password supplied to authenticate user '${IRODS_USERNAME}'" >> ${LOG_LOCATION}
+    iinit "${IRODS_PASSWORD}" 2>> ${LOG_LOCATION}
+    if [ $? -ne 0 ]
     then
         exitWithError
     fi
 else
-    echo "iRODs password not supplied" >> $logLocation
+    echo "iRODs password not supplied" >> ${LOG_LOCATION}
 fi
 
-echo "Executing user command: $*" >> $logLocation
-sh -c "$*"
-exit 0
+execute_user_command
