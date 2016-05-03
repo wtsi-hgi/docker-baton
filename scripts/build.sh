7cc734a6a6a34932539ee1b04bc5073083c65eef
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-05-03 12:02:35 +0100

    Removed convenience scripts as not updated (could be restored and updated later if required).

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIRECTORY/../"
-
-. scripts/_load-args.sh
-
-docker build --build-arg BRANCH="$batonBranch" --build-arg REPOSITORY="$batonRepository" -t "wtsi-hgi/baton:custom-$batonBranch" -f "custom/$irodsVariant/Dockerfile" .
