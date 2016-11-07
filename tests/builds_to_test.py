c1ed1cbe98d112c7f4f3b31a78b160045cd06555
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-11-07 12:08:31 +0000

    Corrects typo in tests.

--- tests/builds_to_test.py
+++ tests/builds_to_test.py
@@ -16,11 +16,11 @@ builds_to_test = [
     (_BATON_WITH_IRODS_3_3_1_BASE, ("mercury/baton:0.16.4-with-irods-3.3.1", "0.16.4/irods-3.3.1")),
     (_BATON_WITH_IRODS_4_1_8_BASE, ("mercury/baton:0.16.4-with-irods-4.1.8", "0.16.4/irods-4.1.8")),
     (_BATON_WITH_IRODS_4_1_9_BASE, ("mercury/baton:0.16.4-with-irods-4.1.9", "0.16.4/irods-4.1.9")),
-    (_BATON_WITH_IRODS_4_1_9_BASE, ("mercury/baton:0.17.0-with-irods-4.1.10", "0.17.0/irods-4.1.10")),
+    (_BATON_WITH_IRODS_4_1_10_BASE, ("mercury/baton:0.17.0-with-irods-4.1.10", "0.17.0/irods-4.1.10")),
     (_BATON_WITH_IRODS_3_3_1_BASE, ("mercury/baton:devel-with-irods-3.3.1", "devel/irods-3.3.1")),
     (_BATON_WITH_IRODS_4_1_9_BASE, ("mercury/baton:devel-with-irods-4.1.8", "devel/irods-4.1.8")),
     (_BATON_WITH_IRODS_4_1_9_BASE, ("mercury/baton:devel-with-irods-4.1.9", "devel/irods-4.1.9")),
-    (_BATON_WITH_IRODS_4_1_9_BASE, ("mercury/baton:devel-with-irods-4.1.10", "devel/irods-4.1.10"))
+    (_BATON_WITH_IRODS_4_1_10_BASE, ("mercury/baton:devel-with-irods-4.1.10", "devel/irods-4.1.10"))
 ]
 
 logging.root.setLevel(logging.DEBUG)
